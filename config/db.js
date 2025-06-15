dacc77cf870e8ff6035c97c5dde8040fc3ad0a69
Rahmaruuxi <rahmaruun442@gmail.com>
2025-06-16 01:33:37 +0300

    Switch database connection from local to MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Connect to MongoDB using the URL from your environment variables
     await mongoose.connect(process.env.MONGO_URL);
+
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
